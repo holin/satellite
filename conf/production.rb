f628944c987cd9a3f5be418ab0fc32422fa77920
Devon <devon.y@gmail.com>
2009-01-21 22:33:56 +0800

    change default page

--- conf/production.rb
+++ conf/production.rb
@@ -1,14 +1,16 @@
 # production configuration
 
 # URI of master git repository for wiki content
-config.master_repository_uri = 'git+ssh://kenpratt@mueller.kenpratt.net/var/git/wiki.kenpratt.net'
+config.master_repository_uri = 'ssh://devony@toadstool.dreamhost.com/home/devony/git/pkm.git'
 
 # user information to use for git content submissions
-config.user_name = 'Ken Pratt'
-config.user_email = 'ken@kenpratt.net'
+config.user_name = 'Devon'
+config.user_email = 'devon.y@gmail.com'
 
 # logging level (:error, :warn, :info, :debug)
-config.log_level = :debug
+config.log_level = :error
 
 # automatically reload app when app files change? (for development)
 config.auto_reload = true
+
+config.server_port = 8888
